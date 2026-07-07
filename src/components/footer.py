9bdfe3962c0c28de9882a8a8f4353dbfe3ef9516
arkeshdev <arkeshumesh07@gmail.com>
2026-07-07 21:01:07 +0530

    streamlit setup + homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+def footer_home():
+
+    st.markdown("""
+        <div style="margin-top: 2rem; display: flex; gap: 6px; justify-content:center; items-align: center; color: rgba(255,255,255,0.85);
+            font-size: 14px;">
+            © 2026 SnapClass | Developed by <strong>Arkesh U</strong>
+            
+        </div>
+                """, unsafe_allow_html=True)
